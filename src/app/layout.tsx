49191cf74070ce517a5c6daf4c8ccccfd7b9d97c
codemine24 <codemine24@gmail.com>
2025-10-22 15:00:04 +0600

    footer added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import { Footer } from "@/components/footer/footer";
+import Navbar from "@/components/navbar/navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-"use client"
+"use client";
 
-import { motion } from "motion/react"
+import {
+  textChild,
+  textContainer,
+} from "@/components/animation/text-animation";
 import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
-import { textChild, textContainer } from "@/components/animation/text-animation";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen w-full flex-col items-start justify-start overflow-hidden">
+    <div className="relative flex w-full flex-col items-start justify-start overflow-hidden">
       <BackgroundRippleEffect />
-      <div className="w-full h-screen flex flex-col items-center justify-center text-center mt-10">
+      <div className="w-full h-[calc(100vh-135px)] flex flex-col items-center justify-center text-center mt-10">
         <motion.h1
-          className="text-7xl md:text-8xl font-semibold w-2/3 flex flex-col gap-4 leading-[1.2] z-10"
+          className="text-7xl md:text-8xl font-semibold w-2/3 flex flex-col gap-2 leading-[1.2] z-10"
           variants={textContainer}
           initial="hidden"
           animate="visible"
@@ -20,30 +23,34 @@ export default function Home() {
               variants={textChild}
               className="inline-block mr-[1rem]"
             >
-              Crafting
+              Customer
             </motion.span>
             <motion.span
               variants={textChild}
               className="inline-block mr-[1rem] animate-gradient bg-gradient-text pb-2"
             >
-              Digital
+              Onboarding
             </motion.span>
           </div>
           <motion.span variants={textChild} className="inline-block">
-            Experiences
+            Made Easy
           </motion.span>
         </motion.h1>
 
-        <motion.div className="mt-4" variants={textContainer} initial="hidden" animate="visible">
+        <motion.div
+          className="mt-4"
+          variants={textContainer}
+          initial="hidden"
+          animate="visible"
+        >
           <motion.p
             className="relative z-10 mx-auto max-w-xl text-center text-neutral-800 dark:text-neutral-500"
             variants={textChild}
             initial="hidden"
             animate="visible"
           >
-            Hover over the boxes above and click.To be used on backgrounds of hero
-            sections OR Call to Action sections. I beg you don&apos;t use it
-            everywhere.
+            Adam Customer Onboarding is a multi-step form that helps you to
+            onboarding your customers. Build your own onboarding form in minutes.
           </motion.p>
         </motion.div>
 
@@ -53,7 +60,7 @@ export default function Home() {
           transition={{ duration: 0.8 }}
           className="relative z-10 mt-8 inline-flex items-center justify-center gap-2 rounded-lg bg-neutral-800 px-10 py-3 text-lg font-medium text-white transition-colors hover:bg-neutral-900 dark:bg-neutral-200 dark:text-neutral-900 dark:hover:bg-neutral-100"
         >
-          Click Me
+          Get Started
         </motion.button>
       </div>
     </div>
